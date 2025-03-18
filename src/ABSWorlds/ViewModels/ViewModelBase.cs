3cfa0be26cb3e643d937043f5baf29789ebf885f
Alex Look <alex.v.look@gmail.com>
2025-03-18 21:59:55 +0300

    Initial solution structure
    Create initial solution structure

---
+++ src/ABSWorlds/ViewModels/ViewModelBase.cs
@@ -0,0 +1,5 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ABSWorlds.ViewModels;
+
+public class ViewModelBase : ObservableObject { }
---
+++ src/ABSWorlds/Views/MainWindow.axaml.cs
@@ -0,0 +1,7 @@
+using Avalonia.Controls;
+
+namespace ABSWorlds.Views;
+
+public partial class MainWindow : Window {
+    public MainWindow() { InitializeComponent(); }
+}

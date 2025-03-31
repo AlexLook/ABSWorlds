8293c0745b919cc8f25aaaf47a68df6be03ba7be
Alex Look <alex.v.look@gmail.com>
2025-04-01 00:11:06 +0300

    Add missed files

---
+++ src/ABSWorlds.Common/Models/Reference.cs
@@ -0,0 +1,6 @@
+﻿namespace ABSWorlds.Common.Models;
+
+public class Reference(string path, string position) {
+    public string Path     { get; init; } = path;
+    public string Position { get; init; } = position;
+}
---
+++ src/ABSWorlds.Editor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace ABSWorlds.Editor.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase {
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
